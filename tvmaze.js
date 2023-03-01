505c25b4f54127db5c5540b52f794490e9ffdc2a
William Squires <william.squiresii@gmail.com>
2023-03-01 14:26:18 -0600

    we can now search for and display shows

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,9 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const searchShowsUrl = "http://api.tvmaze.com/search/shows";
+//http://api.tvmaze.com/search/shows?q=[searchquery]
+//http://api.tvmaze.com/shows/[showid]/episodes
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,32 +15,28 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+async function getShowsByTerm(term) {
+  console.log("getShowsByTerm", term);
+  const params = {params: {q : term}}
+  const showsResponse = await axios.get(searchShowsUrl, params);
+  let showsData = showsResponse.data;
+  return showsData.map( show => makeShowObject(show.show));
+}
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+/** takes a show and returns an object with only its 
+ * id, name, summary, and image. If there is no image,
+ * that value is set to null.
+ */
+function makeShowObject(show) {
+  const id = show.id;
+  const name = show.name;
+  const summary = show.summary;
+  const image = (show.image) ? show.image.original : null;
+  return {id, name, summary, image};
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
-
 function populateShows(shows) {
   $showsList.empty();
 
@@ -46,8 +45,8 @@ function populateShows(shows) {
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+              src="${show.image}" 
+              alt="${show.name}" 
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -67,8 +66,8 @@ function populateShows(shows) {
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchForShowAndDisplay() {
+  console.log("searchForShowAndDisplay ran");
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
 
@@ -78,6 +77,7 @@ async function searchForShowAndDisplay() {
 
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
+  console.log("button was pressed");
   await searchForShowAndDisplay();
 });
 
